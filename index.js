e6519c5e5298081a68180dfc4524aec56bdd7196
Dean Howe <hideanhowe@gmail.com>
2026-01-18 18:30:10 +0000

    solution

--- index.js
+++ index.js
@@ -7,15 +7,41 @@
 // In descending order (highest to lowest), return "Descending".
 // Not sorted in ascending or descending order, return "Not sorted".
 
+// function isSorted(arr) {
+//   // console.log(arr)
+
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     // acending
+//     if (arr[i] < arr[i + 1]) {
+//       return 'Ascending';
+//     }
+//   }
+
+//   // if (arr[i] < arr[i + 1]) {
+//   // return
+//   // }
+//   // return arr;
+// }
+
 function isSorted(arr) {
   // console.log(arr)
 
-  for (let i = 0; i < arr.length; i++) {
+  let isAsc = true;
+  let isDesc = true;
+
+  for (let i = 0; i < arr.length - 1; i++) {
     // acending
-    if (arr[i] < arr[i + 1]) {
-      return 'Ascending';
+    if (arr[i] >= arr[i + 1]) {
+      isAsc = false;
     }
+
+    // descending
+    if (arr[i] <= arr[i + 1]) isDesc = false;
   }
+
+  if (!isAsc && !isDesc) return 'Not sorted';
+  else if (isAsc) return 'Ascending';
+  else return 'Descending';
 
   // if (arr[i] < arr[i + 1]) {
   // return
